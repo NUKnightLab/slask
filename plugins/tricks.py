3bbcdea28e99bbd3709f6638bfeb8b348301ea63
Joe Germuska <joe@germuska.com>
2015-02-10 09:43:41 -0600

    think this is it

--- plugins/tricks.py
+++ plugins/tricks.py
@@ -13,18 +13,23 @@ def on_message(msg, context):
     if not match: return
 
     user_id, command = match.groups()
-    if user_id == 'U03J911F1' or user_id == 'leelou':
-        logging.debug(context['client'].server.users.get(user_id,'no such user id'))
+    user = context['client'].server.users.get(user_id,None)
+    if user['name'] == 'leelou':
         for pat, func in PHRASES:
             if pat.match(command):
-                logging.debug(u"Leelou matched /{}/".format(pat.pattern))
                 return func(command)
-        logging.debug(u"Leelou doesn't know how to {}".format(command))
         return None
 
 def on_presence_change(msg, context):
     if msg.get('presence') == 'active':
-        logging.debug(u"Leelou sees {}".format(msg.get('user',"no user?")))
+        user_id = user_obj = user_name = None
+        user_id = msg.get('user',None)
+        if user_id:
+            user_obj = context['client'].server.users.get(user_id,None)
+            if user_obj:
+                user_name = user_obj.get('name',None)
+        logging.debug(u"Leelou sees {} ({})".format(user_id,user_name))
+
     return None
 
 ROLL_OVER_GIFS = ['http://stream1.gifsoup.com/view4/2110073/french-bulldog-roll-over-o.gif', 'http://www.beheadingboredom.com/wp-content/uploads/2013/04/cat-teaches-dog-trick.gif', 'http://giphy.com/gifs/dog-puppy-tired-NnafYvjXZK9j2', 'http://giphy.com/gifs/cute-sloth-playing-dead-y5owIXKzlPYA0' ]
