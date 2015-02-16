e203f460c847ee6ebe50c789cddf365ea5619d75
Joe Germuska <joe@germuska.com>
2015-02-16 10:43:54 -0600

    fix slack user ID bug

--- plugins/dontshout.py
+++ plugins/dontshout.py
@@ -3,7 +3,9 @@
 def on_message(msg, context):
     """Context has keys: 'client', 'config', 'hooks'"""
     text = msg.get("text", "")
+
     if len(text) <= 5:
         return None
+    text = re.sub(r'<@.+?>','',text) # Slack user IDs look like shouting...
     if text == text.upper():
         return "You don't have to shout."
